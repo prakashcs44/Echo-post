d56d2a3d1094c0a51c471000b26afab0d24cd6df
prakashcs44 <porkashbatarai@gmail.com>
2024-06-16 13:49:35 +0530

    fixed: typo

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -3,10 +3,8 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 const Post = require("../models/post.model");
 const cloudinary = require("cloudinary");
 
-
 exports.getAllPost = catchAsyncError(async (req, res, next) => {
   let posts = await Post.find().populate("user", "name avatar");
-
 
   res.status(200).json({
     success: true,
@@ -14,22 +12,18 @@ exports.getAllPost = catchAsyncError(async (req, res, next) => {
   });
 });
 
-
-
 exports.getPost = catchAsyncError(async (req, res, next) => {
   const { id } = req.params;
 
   let post = await Post.findById(id)
-    .populate('user', 'name avatar')
+    .populate("user", "name avatar")
     .populate({
-        path: 'comments',
-        populate: {
-            path: 'user',
-            select: 'name avatar'
-        }
-    })
-   
- 
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "name avatar",
+      },
+    });
 
   if (!post) {
     return next(new ErrorHandler("Post not found", 404));
@@ -39,26 +33,22 @@ exports.getPost = catchAsyncError(async (req, res, next) => {
 });
 
 exports.addPost = catchAsyncError(async (req, res, next) => {
-  const { content,file } = req.body;
-   const newPost = {content,user:req.user._id};
+  const { content, file } = req.body;
+  const newPost = { content, user: req.user._id };
 
-     if(file&&file!==""){
-     const mycloud = await cloudinary.v2.uploader.upload(file,{
-      folder:"echo_post_user_files",
-      width:150,
-      crop:"scale"
-     })
-     newPost.file = {public_id:mycloud.public_id,url:mycloud.secure_url};
-    }
-  
+  if (file && file !== "") {
+    const mycloud = await cloudinary.v2.uploader.upload(file, {
+      folder: "echo_post_user_files",
+      width: 150,
+      crop: "scale",
+    });
+    newPost.file = { public_id: mycloud.public_id, url: mycloud.secure_url };
+  }
 
-  let post = await Post.create({
-   newPost
-  });
+  let post = await Post.create(newPost);
 
   post = await post.populate("user", "name avatar");
 
- 
   res.status(200).json({ success: true, post });
 });
 
@@ -79,32 +69,35 @@ exports.deletePost = catchAsyncError(async (req, res, next) => {
 });
 
 exports.updatePost = catchAsyncError(async (req, res, next) => {
-  const { postId, content,file } = req.body;
-  
+  const { postId, content, file } = req.body;
+
   let post = await Post.findById(postId);
   if (!post) {
     return next(new ErrorHandler("Post not found", 404));
   }
-  const updatedPost = {content};
+  const updatedPost = { content };
 
   if (file && post.file) {
     await cloudinary.v2.uploader.destroy(post.file.public_id);
-    const mycloud = await cloudinary.v2.uploader.upload(file,{
-      folder:"echo_post_user_files",
-      width:150,
-      crop:"scale"
-     })
-     updatedPost.file = {public_id:mycloud.public_id,url:mycloud.secure_url};
+    const mycloud = await cloudinary.v2.uploader.upload(file, {
+      folder: "echo_post_user_files",
+      width: 150,
+      crop: "scale",
+    });
+    updatedPost.file = {
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
+    };
   }
 
-  post = await Post.findByIdAndUpdate(
-    postId,
-    updatedPost,
-    { new: true, runValidators: true, useFindAndModify: true }
-  );
+  post = await Post.findByIdAndUpdate(postId, updatedPost, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true,
+  });
 
   post = await post.populate("user", "name avatar");
- 
+
   res.status(200).json({ success: true, post });
 });
 
@@ -122,18 +115,16 @@ exports.likePost = catchAsyncError(async (req, res, next) => {
   res.status(200).json({ success: true, post });
 });
 
-
-exports.dislikePost  = catchAsyncError(async (req, res, next) => {
+exports.dislikePost = catchAsyncError(async (req, res, next) => {
   const { postId } = req.body;
   const post = await Post.findById(postId).populate("user", "name avatar");
   if (!post) {
     return next(new ErrorHandler("Post not found", 404));
   }
-  
-  post.likes = post.likes.filter((like)=>!like.equals(req.user._id));
- 
+
+  post.likes = post.likes.filter((like) => !like.equals(req.user._id));
+
   await post.save();
-  
+
   res.status(200).json({ success: true, post });
 });
-
