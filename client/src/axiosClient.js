c49c0e330bb87010777195b09913e80ba2ba356f
prakashcs44 <porkashbatarai@gmail.com>
2024-08-06 09:54:21 +0530

    added request response interceptors

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -11,5 +11,33 @@ const axiosClient = axios.create({
    
   });
 
+  axiosClient.interceptors.request.use(
+    function (config) {
+     
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+
+  axiosClient.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+     
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem("token");
+        window.location = '/login';
+      }
+      return Promise.reject(error);
+    }
+  );
   
 export default axiosClient;
