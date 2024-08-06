f3ffa3847d07a3dbc6821c26159f766df11f0d45
prakashcs44 <porkashbatarai@gmail.com>
2024-08-06 09:36:48 +0530

    sending token directly in response

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,19 +1,13 @@
 const sendToken = (user,statusCode,res)=>{
     const token = user.getJWTToken();
-    const options = {
-       httpOnly:true,
-       expires:new Date(
-           Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
-           ),
-           sameSite:"None",
-           secure:true,
-    }
    
    
    
-    res.status(statusCode).cookie("token",token,options).json({
+   
+    res.status(statusCode).json({
        success:true,
        user,
+       token
     });
    
    }
