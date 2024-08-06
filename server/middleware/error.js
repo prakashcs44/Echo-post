293f00c59cd280aa63e18e742c1bd2f62b2352d4
prakashcs44 <porkashbatarai@gmail.com>
2024-08-06 11:20:02 +0530

    changed status code for jwt

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -17,13 +17,13 @@ module.exports = (err,req,res,next)=>{
   // Wrong JWT error
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again `;
-    err = new ErrorHandler(message, 400);
+    err = new ErrorHandler(message, 401);
   }
 
   // JWT EXPIRE error
   if (err.name === "TokenExpiredError") {
     const message = `Json Web Token is Expired, Try again `;
-    err = new ErrorHandler(message, 400);
+    err = new ErrorHandler(message, 401);
   }
 
   err.statusCode = err.statusCode||500;
